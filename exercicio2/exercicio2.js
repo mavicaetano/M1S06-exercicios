0600dffb72be5e5eccc50866c2c5c81f0f23eb2c
Maria Vicente <mnvcaetano@gmail.com>
2023-07-30 00:14:00 -0300

    Adiciona comentário (enunciado) no exercício

--- exercicio2/exercicio2.js
+++ exercicio2/exercicio2.js
@@ -1,3 +1,9 @@
+/* Crie um array com uma lista de objetos, cada um representando um aluno. Cada aluno deve possuir as propriedades nome (string), 
+nota1 (number) e nota2 (number).
+Utilize uma função de array, como map, filter ou reduce, para calcular a média das notas de cada aluno.
+Crie um novo array contendo objetos com as propriedades nome e media, onde media é a média das notas.
+Exiba o array resultante no console, ordenado em ordem decrescente de média.*/
+
 let aluno = [
     { nome: "Maria", nota1: 8.5, nota2: 9 },
     { nome: "Iuri", nota1: 10, nota2: 9.5 },
